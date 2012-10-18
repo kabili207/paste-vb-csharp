efb43c02be4af4a9513802a0a9d58d9b91080e2b
Andrew Nagle <kabili@zyrenth.com>
2012-10-17 21:44:20 -0400

    Fixed Command status to hide when incompatible files are active. For some reason they still show up as disabled when Visual Studio firsts starts instead of as invisible.

--- PasteAsCSharpVB/NRefactoryConverter.cs
+++ PasteAsCSharpVB/NRefactoryConverter.cs
@@ -9,45 +9,31 @@ using ICSharpCode.NRefactory.PrettyPrinter;
 
 namespace PasteAsCSharpVB
 {
-	class NRefactoryConverter : IConvertCode
+	class NRefactoryConverter
 	{
-
-		public string ConverterName
-		{
-			get { return "NRefactory"; }
-		}
-		public string Convert(string code)
-		{
-			return ConvertCodeSnippet(code, true);
-		}
-
+		
 		public string ConvertCodeSnippet(string codeToConvert, bool csharpToVb)
 		{
-			//StringReader input = new StringReader(codeToConvert);
-			SnippetParser parser = default(SnippetParser);
-			List<ISpecial> specials;
+			// TODO: Will need to expand when other languages are added.
 
-			
-			parser = new SnippetParser((csharpToVb ? SupportedLanguage.CSharp : SupportedLanguage.VBNet));
+			SnippetParser parser = new SnippetParser((csharpToVb ? SupportedLanguage.CSharp : SupportedLanguage.VBNet));
 
 			parser.Parse(codeToConvert);
-			//specials = parser.Lexer.SpecialTracker.RetrieveSpecials();
-
-			//if ((parser.Errors.Count > 0))
-			//{
-			//    return parser.Errors.ErrorOutput;
-			//}
 
 			INode node = parser.Parse(codeToConvert);
+
+			if ((parser.Errors.Count > 0))
+			{
+			    return parser.Errors.ErrorOutput;
+			}
+
 			// parser.Errors.ErrorOutput contains syntax errors, if any
 			// parser.Specials is the list of comments, preprocessor directives etc.
 			if (csharpToVb)
 				PreprocessingDirective.CSharpToVB(parser.Specials);
 			else
 				PreprocessingDirective.VBToCSharp(parser.Specials);
-			// Convert C# constructs to VB.NET:
-			//node.AcceptVisitor(new CSharpConstructsVisitor(), null);
-			//node.AcceptVisitor(new ToVBNetConvertVisitor(), null);
+
 
 			IOutputAstVisitor output = output = (csharpToVb ? (IOutputAstVisitor)new VBNetOutputVisitor() : (IOutputAstVisitor)new CSharpOutputVisitor());
 			using (SpecialNodesInserter.Install(parser.Specials, output))
@@ -55,13 +41,6 @@ namespace PasteAsCSharpVB
 				node.AcceptVisitor(output, null);
 			}
 
-			/*CompilationUnit cu = default(CompilationUnit);
-			IOutputAstVisitor output = default(IOutputAstVisitor);
-			cu = parser.CompilationUnit;
-
-			output = (csharpToVb ? (IOutputAstVisitor)new VBNetOutputVisitor() : (IOutputAstVisitor)new CSharpOutputVisitor());
-			SpecialNodesInserter.Install(specials, output);
-			cu.AcceptVisitor(output, DBNull.Value);*/
 			return output.Text;
 		}
 
